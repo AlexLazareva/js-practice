6dd012b4aa69020eadf2c7d9cd52e883c5946eba
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-10-07 22:06:40 +0300

    create options and script

--- select/script.js
+++ select/script.js
@@ -1,1 +1,6 @@
+function selectFruit() {
+    var sel = document.getElementById('fruits').selectedIndex;
+    var options = document.getElementById('fruits').options;
+    alert('Выбрана опция ' +options[sel].text);
+}
 
