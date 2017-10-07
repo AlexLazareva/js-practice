acb7eb44e70e6d36ed431df627fd15ff02b75fce
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-10-07 21:08:37 +0300

    initial first step

---
+++ input/script.js
@@ -0,0 +1,13 @@
+function fun1 () {
+    var checkbox;
+
+    checkbox = document.getElementById('first');
+
+    if (checkbox.checked) {
+        alert('Checked!');
+    }
+    else {
+        alert('Unchecked!');
+    }
+}
+
