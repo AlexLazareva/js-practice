e45c759778cdcc7508677ec8b2e0665af112aa37
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-10-08 10:16:36 +0300

    create range

--- select/script.js
+++ select/script.js
@@ -1,8 +1,14 @@
 function select() {
     var sel = document.getElementById('fruits').selectedIndex;
     var options = document.getElementById('fruits').options;
-    document.getElementById('text').innerHTML = 'Выбрана опция ' +options[sel].text;
-    //alert('Выбрана опция ' +options[sel].text);
+    document.getElementById('text').innerHTML = 'Выбрана опция ' + options[sel].text;
+}
+
+function range() {
+    var rng = document.getElementById('range');
+    var text = document.getElementById('rangeText');
+
+    text.innerHTML = rng.value;
 }
 
 
