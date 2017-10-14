7fd5e18f6201e5071927812a4ca5cc3ef1dfb066
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-10-14 19:04:46 +0300

    add script

--- tabs/main.js
+++ tabs/main.js
@@ -1,1 +1,9 @@
+function openTab(inside) {
+    var content = document.getElementsByClassName('content');
 
+    for (var i = 0; i < content.length; i++) {
+        content[i].style.display = 'none';
+    }
+
+    document.getElementById(inside).style.display = 'block';
+}
