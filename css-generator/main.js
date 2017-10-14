1900fac5c0ab1d3cd8b22eeb78729b6dc5f1cb32
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-10-14 14:53:22 +0300

    fix error

--- css-generator/main.js
+++ css-generator/main.js
@@ -1,22 +1,22 @@
 function changeBrds() {
     var rtl = document.getElementById('rngTopLeft').value;
-    var ttl = document.getElementById('txtTopLeft').value;
+    var ttl = document.getElementById('txtTopLeft');
 
     var rtr = document.getElementById('rngTopRight').value;
-    var ttr = document.getElementById('txtTopRight').value;
+    var ttr = document.getElementById('txtTopRight');
 
     var rbl = document.getElementById('rngBottomLeft').value;
-    var tbl = document.getElementById('txtBottomLeft').value;
+    var tbl = document.getElementById('txtBottomLeft');
 
     var rbr = document.getElementById('rngBottomRight').value;
-    var tbr = document.getElementById('txtBottomRight').value;
+    var tbr = document.getElementById('txtBottomRight');
 
     var block = document.getElementById('result');
 
     ttl.value = rtl;
-    rtr.value = rtr;
-    rbl.value = tbl;
-    rbr.value = tbr;
+    ttr.value = rtr;
+    tbl.value = rbl;
+    tbr.value = rbr;
 
     block.style.borderRadius = rtl + 'px ' + rtr + 'px ' + rbr + 'px ' + rbl + 'px ';
 }
