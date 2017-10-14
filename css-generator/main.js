b152d5784ab49e0beffb860d0d38a08e746936ed
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-10-14 14:43:45 +0300

    add changeBrds

--- css-generator/main.js
+++ css-generator/main.js
@@ -1,3 +1,22 @@
 function changeBrds() {
-    console.log('change!');
+    var rtl = document.getElementById('rngTopLeft').value;
+    var ttl = document.getElementById('txtTopLeft').value;
+
+    var rtr = document.getElementById('rngTopRight').value;
+    var ttr = document.getElementById('txtTopRight').value;
+
+    var rbl = document.getElementById('rngBottomLeft').value;
+    var tbl = document.getElementById('txtBottomLeft').value;
+
+    var rbr = document.getElementById('rngBottomRight').value;
+    var tbr = document.getElementById('txtBottomRight').value;
+
+    var block = document.getElementById('result');
+
+    ttl.value = rtl;
+    rtr.value = rtr;
+    rbl.value = tbl;
+    rbr.value = tbr;
+
+    block.style.borderRadius = rtl + 'px ' + rtr + 'px ' + rbr + 'px ' + rbl + 'px ';
 }
