004a1156969668631bb3090acc023ecdb7e4d40d
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-10-17 21:50:54 +0300

    add bootstrap css

--- ls-modal/main.js
+++ ls-modal/main.js
@@ -1,17 +1,19 @@
-var modal = document.getElementById('modal');
-var btn = document.getElementById('openModal');
-var closeBtn = document.getElementsByClassName('close')[0];
+window.onload = function () {
+    var modal = document.getElementById('modal');
+    var btn = document.getElementById('openModal');
+    var closeBtn = document.getElementsByClassName('close')[0];
 
-btn.onclick = function () {
-    modal.style.display = 'block';
-};
+    btn.onclick = function () {
+        modal.style.display = 'block';
+    };
 
-closeBtn.onclick = function () {
-    modal.style.display = 'none';
-};
-
-window.onclick = function (event) {
-    if (event.target == modal) {
+    closeBtn.onclick = function () {
         modal.style.display = 'none';
-    }
+    };
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    };
 };
